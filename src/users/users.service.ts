a73215c587cb6d2f4f1b97b9bdc0251c18d90f10
An Nguyen <anndb1903@gmail.com>
2025-11-07 21:29:27 +0700

    find by id

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,7 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { genSaltSync, hashSync } from 'bcryptjs';
 import { User } from './schemas/user.schema';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
@@ -31,8 +31,11 @@ export class UsersService {
     return `This action returns all users`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    if (!mongoose.Types.ObjectId.isValid(id)) return 'not found user';
+    return this.userModel.findOne({
+      _id: id,
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
