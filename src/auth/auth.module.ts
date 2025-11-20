8092c5c7dca4b9dfe87cd58c7e18ac61c100785e
An Nguyen <anndb1903@gmail.com>
2025-11-20 22:35:10 +0700

    new

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { LocalStrategy } from './passport/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './passport/jwt.strategy';
+import ms from 'ms';
 
 @Module({
   providers: [AuthService, LocalStrategy, JwtStrategy],
@@ -15,10 +16,10 @@ import { JwtStrategy } from './passport/jwt.strategy';
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        secret: configService.get<string>('JWT_ACCESS_TOKEN'),
         signOptions: {
-          expiresIn: parseInt(
-            configService.get<string>('JWT_EXPIRE') || '3600',
+          expiresIn: ms(
+            parseInt(configService.get<string>('JWT_ACCESS_EXPIRE') || '1d'),
           ),
         },
       }),
